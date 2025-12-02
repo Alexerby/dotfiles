1b6c43284e596ac177420844b36e553a624c15be
alexerby <mail@alexandereriksson.se>
2025-12-02 23:03:34 +0100

    add: render-markdown

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,27 @@
-return 
-{
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            latex = {
+                enabled = true,
+                render_modes = false,
+                converter = { 'utftex', 'latex2text' },
+                highlight = 'RenderMarkdownMath',
+                position = 'center',
+                top_pad = 0,
+                bottom_pad = 0,
+            },
+        },
+    }
 }
