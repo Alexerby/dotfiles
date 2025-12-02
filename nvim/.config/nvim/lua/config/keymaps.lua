35cb91d81b7cedbc485d72e844cfefbe2d156a06
alexerby <mail@alexandereriksson.se>
2025-12-02 23:02:19 +0100

    rm: autowrap markdown lines

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymap Helper
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+-- local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
+-- local opts = { noremap = true, silent = true }
+local opts = { silent = true }
+
+
 
 -----------------------------------------------------------
 -- General
@@ -11,7 +15,13 @@ vim.g.maplocalleader = " "
 map('i', 'jj', '<ESC>', opts)
 map('n', '<leader>d', '<Cmd>nohlsearch<CR>', opts)
 map('n', '<F4>', ':w<CR>:!python3 %<CR>', opts)
-map('n', '<F5>', ':w<CR>:!python3 % &<CR>', opts)
+map('n', '<F5>', ':w<CR>:!python3 % &<CR><CR>', opts)
 map('n', '<leader>w', '<C-w>', opts)
 -- map('n', '<leader>sv', '<Cmd>:source $MYVIMRC<CR>', opts)
 map('n', '<leader>sv', '<Cmd>echo "Sourcing nvim file"<CR><Cmd>source %<CR>', opts)
+
+
+-----------------------------------------------------------
+-- MarkdownPreview
+-----------------------------------------------------------
+map('n', '<leader>mp', ':MarkdownPreview<CR>', opts)
