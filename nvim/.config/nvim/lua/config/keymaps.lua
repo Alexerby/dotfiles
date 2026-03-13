cf9a8a900322c2d6740a786b5f09b1b6c46412b4
alexerby <mail@alexandereriksson.se>
2026-03-14 00:56:27 +0100

    Consistency in var naming

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Keymap Helper
 -- local map = vim.api.nvim_set_keymap
-local map = vim.keymap.set
+local keymap = vim.keymap.set
 -- local opts = { noremap = true, silent = true }
 local opts = { silent = true }
 
@@ -12,15 +12,13 @@ local opts = { silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-map('i', 'jj', '<ESC>', opts)
-map('n', '<leader>d', '<Cmd>nohlsearch<CR>', opts)
-map('n', '<F4>', ':w<CR>:!python3 %<CR>', opts)
-map('n', '<F5>', ':w<CR>:!python3 % &<CR><CR>', opts)
-map('n', '<leader>w', '<C-w>', opts)
--- map('n', '<leader>sv', '<Cmd>:source $MYVIMRC<CR>', opts)
-map('n', '<leader>sv', '<Cmd>echo "Sourcing nvim file"<CR><Cmd>source %<CR>', opts)
-
-map('n', '<leader>n', ':!open .<CR><CR>')
+keymap('i', 'jj', '<ESC>', opts)
+keymap('n', '<leader>d', '<Cmd>nohlsearch<CR>', opts)
+keymap('n', '<F4>', ':w<CR>:!python3 %<CR>', opts)
+keymap('n', '<F5>', ':w<CR>:!python3 % &<CR><CR>', opts)
+keymap('n', '<leader>w', '<C-w>', opts)
+keymap('n', '<leader>sv', '<Cmd>echo "Sourcing nvim file"<CR><Cmd>source %<CR>', opts)
+keymap('n', '<leader>n', ':!open .<CR><CR>')
 
 
 -----------------------------------------------------------
